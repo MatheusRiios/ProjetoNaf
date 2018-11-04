7c7103344b6ee8e77bf78dafdeb8ba0f03297bfa
MatheusRiios <matheus.rios.oliveira@hotmail.com>
2018-11-03 22:52:04 -0300

    controllersDia

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,9 +1,15 @@
 module.exports = app => {
     app.route('/users')
         .post(app.api.user.save)
-        .get(app.api.user.get)
-        
+        .get(app.api.user.get)            
     app.route('/users/:id')
         .put(app.api.user.save)
         .get(app.api.user.getUserByID)
+
+    /* Dia */
+    app.route('/dia')
+        .post(app.api.adm.dia.save)
+        .get(app.api.adm.dia.get)
+    app.route('/dia/:id')        
+        .delete(app.api.adm.dia.deleteByID)
 }
